93c2db995346927abdf0a6835355cce1e1edb750
Till Blaha <28341894+tblaha@users.noreply.github.com>
2024-01-08 22:41:22 +0100

    adding accumulate function

--- src/pi-protocol.c
+++ src/pi-protocol.c
@@ -47,6 +47,44 @@ void piSendMsg(void * msg_raw, void (*serialWriter)(uint8_t byte)) {
         }
     }
 }
+unsigned int piAccumulateMsg(void * msg_raw, uint8_t * buf) {
+    // cast to uint8_t pointer
+    uint8_t * msg = (uint8_t *) msg_raw;
+
+    unsigned int i = 0;
+
+    // start byte first. Next field (id) will be handled with the usual escape
+    buf[i++] = PI_STX;
+
+    // this is not that nice, because it assume that id and len are 1 byte
+    bool id_sent = false;
+    uint8_t id = *(msg++);
+    uint8_t len = *(msg++);
+
+    while (len-- > 0) {
+        uint8_t byte;
+        if (!id_sent) {
+            byte = id;
+            len++;
+            id_sent = true;
+        } else {
+            byte = *(msg++);
+        }
+        switch(byte) {
+            case PI_STX:
+                buf[i++] = PI_ESC;
+                buf[i++] = PI_STX_ESC;
+                break;
+            case PI_ESC:
+                buf[i++] = PI_ESC;
+                buf[i++] = PI_ESC_ESC;
+                break;
+            default:
+                buf[i++] = byte;
+        }
+    }
+    return i;
+}
 #endif // #if (PI_MODE & PI_TX)
 
 // --- parser ---
@@ -54,10 +92,10 @@ void piSendMsg(void * msg_raw, void (*serialWriter)(uint8_t byte)) {
 __attribute__((unused)) uint8_t piParse(pi_parse_states_t * p, uint8_t byte) {
     uint8_t res = PI_MSG_NONE_ID;
 
-    p->piEscHit = false;
-    p->piState = PI_IDLE;
-    p->msgId = PI_MSG_NONE_ID;
-    p->byteCount = 0;
+    //p->piEscHit = false;
+    //p->piState = PI_IDLE;
+    //p->msgId = PI_MSG_NONE_ID;
+    //p->byteCount = 0;
     p->msgParseResult = PI_PARSE_MSG_NO_ERROR;
 
 #ifdef PI_STATS
